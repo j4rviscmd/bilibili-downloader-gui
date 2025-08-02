3cea7ea59ecc87b6ffc903bea6ac21ff18d11591
j4rviscmd <j4rviscmd@gmail.com>
2025-08-02 20:33:47 +0900

    Installed animate-ui lib.

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,15 @@
+import { Button } from '@/components/ui/button'
 import { useState } from 'react'
+import './App.css'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
-import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
+      <Button>Shadcn button</Button>
       <div>
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
