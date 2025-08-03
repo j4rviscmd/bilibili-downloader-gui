a29df600d67e59c8899aaa0e251ba360ad767141
j4rviscmd <j4rviscmd@gmail.com>
2025-08-04 00:15:47 +0900

    Backendディレクトリ戦略 & ガワ作成

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,14 +1,26 @@
-// Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
-#[tauri::command]
-fn greet(name: &str) -> String {
-    format!("Hello, {}! You've been greeted from Rust!", name)
-}
+pub mod handlers;
 
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
         .plugin(tauri_plugin_opener::init())
-        .invoke_handler(tauri::generate_handler![greet])
+        .invoke_handler(tauri::generate_handler![
+            validate_ffmpeg,
+            //
+            download_ffmpeg,
+        ])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
+}
+
+#[tauri::command]
+async fn validate_ffmpeg() -> bool {
+    // ffmpegの有効性チェック処理
+    true
+}
+
+#[tauri::command]
+async fn download_ffmpeg() -> Result<(), String> {
+    // ffmpegバイナリのダウンロード処理
+    Ok(())
 }
