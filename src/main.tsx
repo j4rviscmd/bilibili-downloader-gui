80f1f44f57c1bbee71eba8fabe57d100628099d2
j4rviscmd <j4rviscmd@gmail.com>
2025-08-03 13:43:25 +0900

    Installed react-router.

--- src/main.tsx
+++ src/main.tsx
@@ -2,13 +2,16 @@ import '@/styles/index.css'
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { Provider } from 'react-redux'
+import { BrowserRouter } from 'react-router'
 import App from './App'
 import { store } from './app/store'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <Provider store={store}>
-      <App />
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
     </Provider>
   </StrictMode>,
 )
