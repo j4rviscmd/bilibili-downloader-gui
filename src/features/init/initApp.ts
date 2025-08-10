048b3ff3639ed6ad55fb83d0371a391ebfa888ca
j4rviscmd <j4rviscmd@gmail.com>
2025-08-10 15:22:54 +0900

    FEでの初期化処理ハンドル

--- src/features/init/initApp.ts
+++ src/features/init/initApp.ts
@@ -1,22 +1,29 @@
 import { store } from '@/app/store'
 import { setInitiated, setProcessingFnc } from '@/features/init/initSlice'
+import { sleep } from '@/lib/utils'
 import { invoke } from '@tauri-apps/api/core'
 
 export const initApp = async () => {
   console.log('Application initialization started')
+  let isInitialized = false
 
-  store.dispatch(setProcessingFnc('ffmpegの存在チェック'))
+  store.dispatch(setProcessingFnc('ffmpegの有効性チェック'))
   const isValidFfmpeg = await invoke<boolean>('validate_ffmpeg')
-  console.log('isValidFfmpeg:', isValidFfmpeg)
   if (isValidFfmpeg) {
-    store.dispatch(setProcessingFnc('ffmpegが見つかりました'))
+    store.dispatch(setProcessingFnc('ffmpegの有効性チェックに成功しました'))
+    isInitialized = true
   } else {
     store.dispatch(setProcessingFnc('ffmpegをダウンロードしています'))
-    await invoke('install_ffmpeg')
+    const isInstalled = await invoke('install_ffmpeg')
+    if (isInstalled) {
+      isInitialized = true
+    }
   }
 
+  await sleep(1000)
   store.dispatch(setInitiated(true))
   console.log('Application initialization completed')
 
-  return true
+  // 初期化処理に失敗
+  return isInitialized
 }
