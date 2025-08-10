cbebff8340b46d1165afb96b798598d3e74c0821
j4rviscmd <j4rviscmd@gmail.com>
2025-08-10 15:40:53 +0900

    eslint設定

--- eslint.config.js
+++ eslint.config.js
@@ -1,9 +1,9 @@
 import js from '@eslint/js'
-import globals from 'globals'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
-import tseslint from 'typescript-eslint'
 import { globalIgnores } from 'eslint/config'
+import globals from 'globals'
+import tseslint from 'typescript-eslint'
 
 export default tseslint.config([
   globalIgnores(['dist']),
@@ -18,6 +18,12 @@ export default tseslint.config([
     languageOptions: {
       ecmaVersion: 2020,
       globals: globals.browser,
+    },
+    rules: {
+      // Allow custom dependency management in hooks across the project
+      'react-hooks/exhaustive-deps': 'off',
+      // Keep core hooks rules enabled for safety
+      'react-hooks/rules-of-hooks': 'error',
     },
   },
 ])
