b8480a5fd23d5c8f9d66d9da1aa443a8ee3d84bb
j4rviscmd <j4rviscmd@gmail.com>
2025-08-17 21:19:26 +0900

    進捗UIのスタイル見直し

--- src/components/lib/Progress/ProgressStatusBar.tsx
+++ src/components/lib/Progress/ProgressStatusBar.tsx
@@ -18,10 +18,12 @@ function ProgressStatusBar({ progress }: Props) {
       {progress.filesize ? (
         <>
           <div className="flex items-center justify-between">
-            <ProgressLabel className="flex w-full items-center justify-between text-sm">
-              <div>経過: {progress.elapsedTime}s</div>
-              <div>速度: {progress.transferRate.toFixed(1)}MB/s</div>
-              <div>
+            <ProgressLabel className="flex w-full items-center text-sm">
+              <div className="w-1/3">経過: {progress.elapsedTime}s</div>
+              <div className="w-1/3">
+                速度: {progress.transferRate.toFixed(1)}MB/s
+              </div>
+              <div className="w-1/3">
                 {progress.downloaded.toFixed(1)}mb/
                 {progress.filesize.toFixed(1)}mb
               </div>
