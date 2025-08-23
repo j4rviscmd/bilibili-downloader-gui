e06fae8f81bf0dabc6ad3bb23eba08f72834d768
j4rviscmd <j4rviscmd@gmail.com>
2025-08-23 16:39:55 +0900

    AppBarの`ログインユーザ`ラベルのマルチ言語化

--- src/components/lib/AppBar/AppBar.tsx
+++ src/components/lib/AppBar/AppBar.tsx
@@ -1,6 +1,7 @@
 import ToggleThemeButton from '@/features/preference/ToggleThemeButton'
 import LanguagesDropdown from '@/shared/settings/LanguagesDropdown'
 import type { User } from '@/shared/user'
+import { useTranslation } from 'react-i18next'
 
 type Props = {
   user: User
@@ -10,11 +11,14 @@ type Props = {
 
 function AppBar({ user, theme, setTheme }: Props) {
   const userName = user.data.uname
+  const { t } = useTranslation()
 
   return (
     <div className="bg-accent box-border flex h-9 w-full items-center justify-between p-3 shadow-md">
       <div>
-        <span className="text-muted-foreground">ログインユーザ:</span>
+        <span className="text-muted-foreground">
+          {t('app.logged_in_user')}:
+        </span>
         <span className="px-3">{userName}</span>
       </div>
       <div className="flex items-center">
